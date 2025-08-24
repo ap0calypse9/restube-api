23a7ea3923c0cfc0547af9bf0b5bd85dda24379b
ap0calypse9 <logantrek@gmail.com>
2025-08-25 00:32:14 +0530

    add user account management routes including change password, current user, update account, avatar, cover image, and user channel profile

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { loginUser, logoutUser,refreshAccessToken} from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentuser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser,refreshAccessToken, updateAccountDetails, updateAvatar, updateUserCoverImage} from "../controllers/user.controller.js";
 import { registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verify } from "jsonwebtoken";
 //import { UploadStream } from "cloudinary";
 
 
@@ -27,5 +28,12 @@ router.route("/login").post(loginUser)
 //secured routes 
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentuser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"), updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 export default router
